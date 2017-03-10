5634919433a2bed36dcdd2700a119d96acdca075
VidStarc <vid.starc9@gmail.com>
2017-03-10 18:04:37 +0100

    done with my job, it's your turn Neža ;)

--- server.R
+++ server.R
@@ -8,72 +8,100 @@ library(plotly)
 shinyServer(function(input, output) {
   
   output$graf<-renderPlotly(
+    if(input$prim==FALSE){
     if(input$izbira2=='današnja'){
       if(input$izbira=="Vse"){plot_ly(x=price$Epic,y=price$`TodayChg(%)`,type='bar',marker=list(color='red')) %>% 
-                            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"))}
+                            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %")
+                                   ,plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
         else{if(input$izbira=='Samo pozitivne spremembe'){plot_ly(x=price$Epic[which(price$`TodayChg(%)`>=0)],
                                                               y=price$`TodayChg(%)`[price$`TodayChg(%)`>=0],type='bar',marker=list(color='red'))%>% 
-                                                      layout(title="Današnje spremembe različnih delnic")}
-          else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`TodayChg(%)`<0 & price$`TodayChg(%)`>-0.5)],
-                                                     y=price$`TodayChg(%)`[price$`TodayChg(%)`<0 & price$`TodayChg(%)`>-0.5],
+                                                      layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
+          else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`TodayChg(%)`<0 & price$`TodayChg(%)`>-1)],
+                                                     y=price$`TodayChg(%)`[price$`TodayChg(%)`<0 & price$`TodayChg(%)`>-1],
                                                      type='bar',marker=list(color='red'))%>% 
-                                                      layout(title="Današnje spremembe različnih delnic")}
+                                                      layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
 
             else{plot_ly(x=price$Epic[which(price$`TodayChg(%)`<0)],
                      y=price$`TodayChg(%)`[price$`TodayChg(%)`<0],type='bar',marker=list(color='red'))%>% 
-                      layout(title="Današnje spremembe različnih delnic")}}}} 
+                      layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}}}} 
     else{
       if(input$izbira2=='7 dnevna'){
         if(input$izbira=='Vse'){plot_ly(x=price$Epic,y=price$`7DayChg(%)`,type='bar',marker=list(color='red')) %>% 
-          layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"))}
+          layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
           
           else{if(input$izbira=='Samo pozitivne spremembe'){plot_ly(x=price$Epic[which(price$`7DayChg(%)`>=0)],
                                                                 y=price$`7DayChg(%)`[price$`7DayChg(%)`>=0],type='bar',marker=list(color='red'))%>% 
-          layout(title="Današnje spremembe različnih delnic")}
-        else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`7DayChg(%)` <0 & price$`7DayChg(%)`>-0.5)],
-                                          y=price$`7DayChg(%)`[price$`7DayChg(%)` <0 & price$`7DayChg(%)`>-0.5],
+          layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
+        else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`7DayChg(%)` <0 & price$`7DayChg(%)`>-1)],
+                                          y=price$`7DayChg(%)`[price$`7DayChg(%)` <0 & price$`7DayChg(%)`>-1],
                                           type='bar',marker=list(color='red'))%>% 
-            layout(title="Današnje spremembe različnih delnic")}
+            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
           
           else{plot_ly(x=price$Epic[which(price$`7DayChg(%)`<0)],
                        y=price$`7DayChg(%)`[price$`7DayChg(%)`<0],type='bar',marker=list(color='red'))%>% 
-              layout(title="Današnje spremembe različnih delnic")}}}
+              layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}}}
       }
       else{if(input$izbira2=='30 dnevna'){
         if(input$izbira=='Vse'){plot_ly(x=price$Epic,y=price$`30DayChg(%)`,type='bar',marker=list(color='red')) %>% 
-          layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"))}
+          layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
         
         else{if(input$izbira=='Samo pozitivne spremembe'){plot_ly(x=price$Epic[which(price$`30DayChg(%)`>=0)],
                                                                   y=price$`30DayChg(%)`[price$`30DayChg(%)`>=0],type='bar',marker=list(color='red'))%>% 
-            layout(title="Današnje spremembe različnih delnic")}
-          else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`30DayChg(%)` <0 & price$`30DayChg(%)`>-0.5)],
-                                            y=price$`30DayChg(%)`[price$`30DayChg(%)` <0 & price$`30DayChg(%)`>-0.5],
+            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
+          else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`30DayChg(%)` <0 & price$`30DayChg(%)`>-1)],
+                                            y=price$`30DayChg(%)`[price$`30DayChg(%)` <0 & price$`30DayChg(%)`>-1],
                                             type='bar',marker=list(color='red'))%>% 
-              layout(title="Današnje spremembe različnih delnic")}
+              layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
             
             else{plot_ly(x=price$Epic[which(price$`30DayChg(%)`<0)],
                          y=price$`30DayChg(%)`[price$`30DayChg(%)`<0],type='bar',marker=list(color='red'))%>% 
-                layout(title="Današnje spremembe različnih delnic")}}}}
+                layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}}}}
         else{
           if(input$izbira=='Vse'){plot_ly(x=price$Epic,y=price$`6MonthChg(%)`,type='bar',marker=list(color='red')) %>% 
-            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"))}
+            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
           
           else{if(input$izbira=='Samo pozitivne spremembe'){plot_ly(x=price$Epic[which(price$`6MonthChg(%)`>=0)],
                                                                     y=price$`6MonthChg(%)`[price$`6MonthChg(%)`>=0],type='bar',marker=list(color='red'))%>% 
-              layout(title="Današnje spremembe različnih delnic")}
-            else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`6MonthChg(%)` <0 & price$`6MonthChg(%)`>-0.5)],
-                                              y=price$`6MonthChg(%)`[price$`6MonthChg(%)` <0 & price$`6MonthChg(%)`>-0.5],
+              layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
+            else{if(input$neg1==TRUE){plot_ly(x=price$Epic[which(price$`6MonthChg(%)` <0 & price$`6MonthChg(%)`>-1)],
+                                              y=price$`6MonthChg(%)`[price$`6MonthChg(%)` <0 & price$`6MonthChg(%)`>-1],
                                               type='bar',marker=list(color='red'))%>% 
-                layout(title="Današnje spremembe različnih delnic")}
+                layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
               
               else{plot_ly(x=price$Epic[which(price$`6MonthChg(%)`<0)],
                            y=price$`6MonthChg(%)`[price$`6MonthChg(%)`<0],type='bar',marker=list(color='red'))%>% 
-                  layout(title="Današnje spremembe različnih delnic")}}}}
+                  layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}}}}
         }
       
-    }
-      
+    }}
+   else{tab<-price
+          if (length(input$podjetja) == 1) {
+            tab <- tab %>% filter(Epic == input$podjetja)
+          } else {
+            tab <- tab %>% filter(Epic %in% input$podjetja)
+          }
+     if(input$izbira2=='današnja'){
+          plot_ly(x=input$podjetja,y=tab$`TodayChg(%)`,type='bar',marker=list(color='red'))%>% 
+            layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')
+     }
+   else{
+     if(input$izbira2=='7 dnevna'){
+       plot_ly(x=input$podjetja,y=tab$`7DayChg(%)`,type='bar',marker=list(color='red'))%>% 
+         layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')
+     }
+     else{
+       if(input$izbira2=='30 dnevna'){
+         plot_ly(x=input$podjetja,y=tab$`30DayChg(%)`,type='bar',marker=list(color='red'))%>% 
+           layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')
+       }
+       else{plot_ly(x=input$podjetja,y=tab$`6MonthChg(%)`,type='bar',marker=list(color='red'))%>% 
+           layout(title="Spremembe cen delnic podjetij",yaxis=list(title="Sprememba cene delnice v %"),plot_bgcolor='#ADD8E6',paper_bgcolor='#ADD8E6')}
+     }
+   }
+     }   
   )
+  
+  
   
   output$hist<-renderPlot({
     if(input$izbira2=='današnja'){x<-price$`TodayChg(%)`
--- ui.R
+++ ui.R
@@ -1,33 +1,39 @@
 library(plotly)
 library(shinythemes)
+#ADD8E6
+#49C9BA
+
 shinyUI(
-  navbarPage(theme = shinytheme("flatly"),
+  navbarPage(
+    
+    theme = shinytheme("flatly"),
             "Vizualizacija",
              tabPanel("Spremembe",
-                      titlePanel("Primerjava sprememb cen delnic podjetij v FTSE 100"),
+                      titlePanel("Primerjava sprememb cen delnic podjetij v FTSE 100",
+                                 list(tags$head(tags$style("body {background-color: #ADD8E6; }")))),
                       br(),
                       fluidRow(
-                        column(5,wellPanel(
-                        fluidRow(column(6,radioButtons("izbira","Tip spremembe cene",c("Vse","Samo pozitivne spremembe","Samo negativne spremembe")),
-                               conditionalPanel(condition = "input.izbira == 'Samo negativne spremembe'",checkboxInput("neg1","večje od -0.5"))),
-                               column(6,radioButtons("izbira2","Časovno obdobje spremembe",c("današnja","7 dnevna","30 dnevna","6 mesečna"))),
-                               br(),
-                               helpText("za koliko se je spremenila cena delnice podjetja vključenega v indeks FTSE 100 v slednjem časovnem obdobju (v %)"))
+                        column(7,plotlyOutput("graf")),
+                        column(5,wellPanel(tags$style(".well {background-color: springgreen;}"),
+                        fluidRow(column(7,radioButtons("izbira","Tip spremembe cene",c("Vse","Samo pozitivne spremembe","Samo negativne spremembe")),
+                               conditionalPanel(condition = "input.izbira == 'Samo negativne spremembe'",checkboxInput("neg1","večje od -1"))),
+                               column(5,radioButtons("izbira2","Časovno obdobje spremembe",c("današnja","7 dnevna","30 dnevna","6 mesečna"))),
+                               br(),br(),br(),br(),br(),br(),br(),br(),br(),
+                               column(12,helpText("Za koliko se je spremenila cena delnice podjetja vključenega v indeks FTSE 100 v slednjem časovnem obdobju (v %)."))),
+                        checkboxInput("prim","Primerjava podjetij"),
+                        conditionalPanel(condition = "input.prim ",selectInput("podjetja","Podjetja",choices = price$Epic,multiple=TRUE)),
+                        helpText("Izberi podjetja, ki jih želiš primerjati. Primerjava temelji na spremembi cen v določenem časovnem odbdobju. Tip spremembe cene pusti na: Vse, časovno obdobje lahko spreminjaš"))
                         )
                         ),
-                      
-                        column(7,plotlyOutput("graf"))
-                        ),
-                      fluidRow(column(5),
-                      column(7,plotOutput("hist"),
-                               radioButtons("bins","število stolpcev:",choices = c(10,15,20,25,30,50,100),selected=10,inline=TRUE),
-                               checkboxInput("gostota","Prikaži gostoto",value=FALSE)))
+                      br(),
+                      fluidRow(
+                      column(7,plotOutput("hist")),
+                      column(5,wellPanel(
+                        sliderInput("bins","število stolpcev:",min=10,max=100,value=10),
+                             checkboxInput("gostota","Prikaži gostoto",value=FALSE))))
+                               
       
              ),
-            tabPanel("Primerjava",
-                     titlePanel("Primerjava sprememb cen delnic"),
-                     br(),
-                     radioButtons("izbira2","Časovno obdobje spremembe",c("današnja","7 dnevna","30 dnevna","6 mesečna"),inline=TRUE)
-                     )
+            tabPanel("Aplikacija-Neža")
   )
 )
